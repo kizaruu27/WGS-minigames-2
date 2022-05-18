65733e1a9a17ce026843f88e080aac0fd43dbffe
Murran <nfmurran@staff.sfcerenity.org>
2022-05-18 15:39:08 +0700

    Add Creating Room

--- WGS Minigame 2 - Waffle Hunter/Assets/Script/Networking/Launcher.cs
+++ WGS Minigame 2 - Waffle Hunter/Assets/Script/Networking/Launcher.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using TMPro;
+using Photon.Realtime;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
@@ -17,6 +18,15 @@ public class Launcher : MonoBehaviourPunCallbacks
     public TMP_Text loadingText;
 
     public GameObject menuButtons;
+
+    public GameObject createRoomScreen;
+    public TMP_InputField roomNameInput;
+
+    public GameObject roomScreen;
+    public TMP_Text roomNameText;
+
+    public GameObject errorScreen;
+    public TMP_Text errorText;
 
     // Start is called before the first frame update
     void Start()
@@ -33,6 +43,9 @@ public class Launcher : MonoBehaviourPunCallbacks
     {
         loadingScreen.SetActive(false);
         menuButtons.SetActive(false);
+        createRoomScreen.SetActive(false);
+        roomScreen.SetActive(false);
+        errorScreen.SetActive(false);
     }
 
     public override void OnConnectedToMaster()
@@ -49,9 +62,62 @@ public class Launcher : MonoBehaviourPunCallbacks
         menuButtons.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenRoomCreate()
     {
-        
+        CloseMenu();
+        createRoomScreen.SetActive(true);
+    }
+
+    public void CreateRoom()
+    {
+        if (!string.IsNullOrEmpty(roomNameInput.text))
+        {
+            RoomOptions options = new RoomOptions();
+            options.MaxPlayers = 4;
+
+            PhotonNetwork.CreateRoom(roomNameInput.text, options);
+
+            CloseMenu();
+            loadingText.text = "Creating Room...";
+            loadingScreen.SetActive(true);
+        }
+    }
+
+    public override void OnJoinedRoom()
+    {
+        //base.OnJoinedRoom();
+        CloseMenu();
+        roomScreen.SetActive(true);
+
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        //base.OnCreateRoomFailed(returnCode, message);
+        CloseMenu();
+        errorScreen.SetActive(true);
+        errorText.text = "Failed To Create Room: " + message;
+    }
+
+    public void ClosedErrorScreen()
+    {
+        CloseMenu();
+        menuButtons.SetActive(true);
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        loadingScreen.SetActive(true);
+        loadingText.text = "Leaving room...";
+        CloseMenu();
+    }
+
+    public override void OnLeftRoom()
+    {
+        //base.OnLeftRoom();
+        CloseMenu();
+        menuButtons.SetActive(true);
     }
 }

e5d7fd595443fe25d5e9e099bab6fba290dd655c
Dionovan Ramadhani <80899280+nyanboy1127@users.noreply.github.com>
2022-06-02 12:02:03 +0700

    new add waffle system -not using scriptable object

--- WGS Minigame 2 - Waffle Hunter/Assets/Script/Waffle Manager/WaffleHandler.cs
+++ WGS Minigame 2 - Waffle Hunter/Assets/Script/Waffle Manager/WaffleHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WaffleHandler : MonoBehaviour
 {
@@ -8,10 +9,18 @@ public class WaffleHandler : MonoBehaviour
     ShieldHandler handler;
     Animator _anim;
 
+    [Header("UI Component")]
+    [SerializeField] Text waffleTextUI;
+
     private void Awake()
     {
         handler = GetComponent<ShieldHandler>();
         _anim = GetComponentInChildren<Animator>();
+    }
+
+    private void Start()
+    {
+        waffleTextUI.text = "Waffle Collected: " + waffle.ToString();
     }
 
 
@@ -26,11 +35,17 @@ public class WaffleHandler : MonoBehaviour
                 Debug.Log("Kena Player");
             }
         }
+
+        if (other.tag == "Waffle")
+        {
+            IncreaseWaffle();
+        }
     }
 
     public void IncreaseWaffle()
     {
         waffle++;
+        waffleTextUI.text = "Waffle Collected: " + waffle.ToString();
     }
 
     public void DecreaseWaffle()

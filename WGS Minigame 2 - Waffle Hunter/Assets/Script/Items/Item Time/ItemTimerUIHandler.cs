936903bf13a5baf472d18e31b650e60ee69dada6
Dionovan Ramadhani <80899280+nyanboy1127@users.noreply.github.com>
2022-06-12 18:10:20 +0700

    update: item UI handler

--- WGS Minigame 2 - Waffle Hunter/Assets/Script/Items/Item Time/ItemTimerUIHandler.cs
+++ WGS Minigame 2 - Waffle Hunter/Assets/Script/Items/Item Time/ItemTimerUIHandler.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Pun;
 
 public class ItemTimerUIHandler : MonoBehaviour
 {
@@ -12,6 +13,19 @@ public class ItemTimerUIHandler : MonoBehaviour
     [SerializeField] float time;
     public bool isActive;
 
+    PhotonView pv;
+
+    private void Awake()
+    {
+        pv = GetComponent<PhotonView>();
+    }
+
+    private void Start() 
+    {
+        UITimer = GameObject.FindGameObjectWithTag("Timer UI");
+        textMessage = GameObject.FindGameObjectWithTag("Item Message").GetComponent<Text>();
+        textTimer = GameObject.FindGameObjectWithTag("Item Time").GetComponent<Text>();   
+    }
 
     void Update()
     {
@@ -19,11 +33,11 @@ public class ItemTimerUIHandler : MonoBehaviour
        {
            ActivateUITimer();
        }
-
-        if (time <= 0)
-        {
+       if (time <= 0)
+       {
             DeactivateTimer();
-        }
+       }
+
     }
 
     private void OnTriggerEnter(Collider col)
@@ -52,22 +66,29 @@ public class ItemTimerUIHandler : MonoBehaviour
 
     void ActivateUITimer()
     {
-
-        UITimer.SetActive(true);
-    
-        time -= Time.deltaTime;
-        float seconds = Mathf.FloorToInt(time % 60);
-        textTimer.text = seconds.ToString();
+        if (pv.IsMine)
+        {
+            //UITimer.SetActive(true);
         
+            time -= Time.deltaTime;
+            float seconds = Mathf.FloorToInt(time % 60);
+            textTimer.text = seconds.ToString();
+            
 
-        UITimer.SetActive(true);
-        textMessage.text = message;
+            //UITimer.SetActive(true);
+            textMessage.text = message;
+        }
     }
 
     void DeactivateTimer()
     {
-        time = 0;
-        UITimer.SetActive(false);
-        isActive = false;
+        if (pv.IsMine)
+        {
+            time = 0;
+            //UITimer.SetActive(false);
+            textMessage.text = null;
+            textTimer.text = null;
+            isActive = false;
+        }
     }
 }

7b41b3c445cae1ff28bf5ca24d40183feb5b6ded
shantaufiq <shantaufiq@student.telkomuniversity.ac.id>
2022-06-13 09:25:15 +0700

    Add: spawn item to all player

--- WGS Minigame 2 - Waffle Hunter/Assets/Script/Spawner/ItemSpawner.cs
+++ WGS Minigame 2 - Waffle Hunter/Assets/Script/Spawner/ItemSpawner.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
 
 public class ItemSpawner : MonoBehaviour
 {
     [SerializeField] GameObject[] Items;
     [SerializeField] float spawnTime;
+
+    PhotonView view;
+
+    private void Awake() => view = GetComponent<PhotonView>();
 
     // Start is called before the first frame update
     void Start()
@@ -20,11 +26,14 @@ public class ItemSpawner : MonoBehaviour
     IEnumerator SpawnItems()
     {
         int spawnIndex = Random.Range(0, Items.Length);
-
-        Instantiate(Items[spawnIndex].gameObject, transform.position, Quaternion.identity);
+        view.RPC("SpawnToAll", RpcTarget.AllBuffered, spawnIndex);
 
         yield return new WaitForSeconds(spawnTime);
 
         StartCoroutine(SpawnItems());
     }
+
+    [PunRPC]
+    void SpawnToAll(int spawnIndex) =>
+        Instantiate(Items[spawnIndex].gameObject, transform.position, Quaternion.identity);
 }

5f90dc81c2e123a4389160cdd2c63760f0d6126b
shantaufiq <shantaufiq@student.telkomuniversity.ac.id>
2022-06-21 14:48:17 +0700

    fix bug: another player can see item pointer

--- WGS Minigame 2 - Waffle Hunter/Assets/Script/Behaviour/DestroyGameobject.cs
+++ WGS Minigame 2 - Waffle Hunter/Assets/Script/Behaviour/DestroyGameobject.cs
@@ -12,7 +12,7 @@ public class DestroyGameobject : MonoBehaviour
         Destroy(gameObject, destroyTime);
     }
 
-    private void OnTriggerEnter(Collider other) 
+    private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
